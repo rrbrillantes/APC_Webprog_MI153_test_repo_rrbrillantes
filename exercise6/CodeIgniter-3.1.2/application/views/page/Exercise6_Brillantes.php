bfe4c517810730cd5287a0985f557b968fb38412
Rachel Anne Brillantes <Momoxielle@gmail.com>
2016-12-05 22:32:26 +0800

    created views/page folder

---
+++ exercise6/CodeIgniter-3.1.2/application/views/page/Exercise6_Brillantes.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/style.css"/>
+<style>
+	img {
+		margin-left: 40px;
+		margin-top: 40px;
+	}
+	body {
+		background-image: url("<?php echo base_url('css/uploads/bg.jpg')?>");
+		background-attachment: fixed;
+		table border:0;
+		align:center; 
+		cellpadding:15;
+		font-family: Garamond;
+		font-size:20px;
+	}
+	
+	.all {
+		align: center;
+		position: relative;
+	}
+</style>
+<body>
+<center>
+<img src="<?php echo base_url('css/uploads/dream.jpg')?>" style="float:left" height=300;>
+
+<center>
+</div>
+</body>
+</html>
