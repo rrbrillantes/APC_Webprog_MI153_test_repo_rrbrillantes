d82213117f9bd74103b2fab77b2793b3dd34fe26
Rachel Anne Brillantes <Momoxielle@gmail.com>
2016-12-06 13:14:54 +0800

    Created exercise6/CodeIgniter-3.1.2/application/views/templates/headerpage.php

---
+++ exercise6/CodeIgniter-3.1.2/application/views/templates/headerpage.php
@@ -0,0 +1,79 @@
+<html>
+<head>
+</head>
+<style>
+
+	.bb {
+		margin-left: 10px;
+		padding: 10px;
+	}
+	
+	div.fixed {
+		position: fixed;
+	}
+	
+	.hehe {
+		margin-left: 840px;
+	}
+	
+	.back {
+		position: fixed;
+		
+	}
+	
+	#head {
+		position: fixed;
+		margin-left: 450px;
+	}
+</style>
+<img class="back" src="<?php echo base_url('css/uploads/bgback.png')?>" width ="1270" height ="150"/>
+<center>
+<img class="all" src="<?php echo base_url('css/uploads/ALL.png')?>"  width ="500" height ="150"/>
+
+<br>
+<br>
+</center>
+<div class="fixed">
+<img src="<?php echo base_url('css/uploads/vid1.gif')?>" style="float:left" height=300;>
+<img class="hehe" src="<?php echo base_url('css/uploads/vid2.gif')?>" style="float:right" height=300 >
+
+<center>
+<div id ="head">
+	<a href="<?php echo site_url('page/Exercise6_Brillantes'); ?>">
+	<img class="bb" src="<?php echo base_url('css/uploads/HOME.png')?>" width ="100" height ="60"align ="center"/>
+	</a> 
+	<a href="<?php echo site_url('page/personalinfo'); ?>">
+	<img class="bb" src="<?php echo base_url('css/uploads/BASIC.png')?>" width ="100" height ="60"align ="center"/>
+	</a>
+	<a href="<?php echo site_url('page/family'); ?>" >
+	<img class="bb" src="<?php echo base_url('css/uploads/FAM.png')?>" width ="100" height ="60"align ="center"/>
+	</a>
+
+	<br>
+	
+	<a href="<?php echo site_url('page/educ'); ?>">
+	<img class="bb" src="<?php echo base_url('css/uploads/EDUC.png')?>" width ="100" height ="60"align ="center"/>
+	</a> 
+	<a href="<?php echo site_url('page/hobbies'); ?>">
+	<img class="bb" src="<?php echo base_url('css/uploads/HOBBIES.png')?>" width ="100" height ="60"align ="center"/>
+	</a> 
+	<a href="<?php echo site_url('page/favorites'); ?>" >
+	<img class="bb" src="<?php echo base_url('css/uploads/FAVE.png')?>" width ="100" height ="60"align ="center"/>
+	</a>
+
+	<br>
+	
+	<a href="<?php echo site_url('page/trivias'); ?>" target="_self" align >
+	<img class="bb" src="<?php echo base_url('css/uploads/TRIVIAS.png')?>" width ="100" height ="60"align ="center"/>
+	</a>
+	<a href="<?php echo site_url('page/contact'); ?>">
+	<img class="bb" src="<?php echo base_url('css/uploads/CONTACT.png')?>" width ="100" height ="60"align ="center"/>
+	</a>
+	<a href="<?php echo site_url('form/create'); ?>">
+	<img class="bb" src="<?php echo base_url('css/uploads/FORM.png')?>" width ="100" height ="60"align ="center"/>
+	</a>
+	
+	<br>
+	
+</center>
+</div>
