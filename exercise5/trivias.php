4c1092e00a62f08a29dc21deabfdd92c9d658219
Rachel Anne Brillantes <Momoxielle@gmail.com>
2016-12-19 05:39:00 +0800

    edited trivias.php

--- exercise5/trivias.php
+++ exercise5/trivias.php
@@ -45,10 +45,12 @@
 		text-align:left;
 		width: 50%;
 	}
+	
 	img {
 		margin-right: 20px;
 		margin-top: 20px;
 	}
+	
 	body {
 		background-image: url("bg.jpg");
 		background-attachment: fixed;
@@ -63,12 +65,17 @@
 		align: center;
 		position: relative;
 	}
+	
 </style>
 <body>
+
 <center>
+
 <img class="all" src="ALL.png" width ="500" height ="130"/>
 <br>
+
 <center>
+
 <div id ="head">
 	
 	<a href="Exercise5_Brillantes.php" target="_self" align >
