dd58cd6dfa23723c0159ae9b27d09a95ad65734a
Rachel Anne Brillantes <Momoxielle@gmail.com>
2016-12-19 05:17:18 +0800

    added .php file

---
+++ exercise6/exercise5/index.php
@@ -0,0 +1,195 @@
+<?php
+include_once 'dbconfig.php';
+
+// delete condition
+if(isset($_GET['delete_id']))
+{
+ $sql_query="DELETE FROM users WHERE user_id=".$_GET['delete_id'];
+ mysqli_query($link, $sql_query);
+ header("Location: $_SERVER[PHP_SELF]");
+}
+// delete condition
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+
+	<style>
+
+	
+	footer {
+		font-family: Times New Roman;
+		font-size: 17px;
+		font-color: white;
+	}
+	p.con, .adddata{
+		font-size: 40px;
+		color: BLACK;
+		font-family: Gigi;
+		text-align: center;
+		font-weight: bold;
+	}
+	
+	.error {
+		color: #FF0000;
+		font-size:20px;
+	}
+	img {
+		margin-right: 20px;
+		margin-top: 20px;
+	}
+	body {
+		background-image: url("bg.jpg");
+		background-attachment: fixed;
+		table border:0;
+		align:center; 
+		cellpadding:15;
+		font-family: Garamond;
+		font-size:20px;
+	}
+	
+	#eto{
+		width: 1100px;
+		height: 1000px;
+		background: rgba(0,0,0,0.1);
+		padding: 5px;
+		color: black;
+		margin: auto;
+		margin-top: 5px;
+		margin-bottom: 5px;
+		font-family: Garamond;
+		font-size:20px;
+		vertical-align:baseline;
+	}
+
+	#hey {
+		vertical-align:baseline;
+		padding-left:80px;
+		font-family: Garamond;
+		font-size:20px;
+		text-align:center;
+	}
+	
+	table, th, td {
+		border-style: double;
+		border-collapse: collapse;
+		text-align:center;
+		font-size:15px;
+		padding:5px;
+}
+</style>
+<body>
+<center>
+<img class="all" src="ALL.png" width ="500" height ="130"/>
+<br>
+<center>
+<div id ="head">
+	
+	<a href="Exercise5_Brillantes.php" target="_self" align >
+	<img src="HOME.png" width ="80" height ="40"align ="center"/>
+	</a> 
+	<a href="personalinfo.php" target="_self" align >
+	<img src="BASIC.png" width ="80" height ="40"align ="center"/>
+	</a>
+	<a href="family.php" target="_self" align >
+	<img src="FAM.png" width ="80" height ="40"align ="center"/>
+	</a>
+	<a href="educ.php" target="_self" align >
+	<img src="EDUC.png" width ="80" height ="40"align ="center"/>
+	</a> 
+	<a href="hobbies.php" target="_self" align >
+	<img src="HOBBIES.png" width ="80" height ="40"align ="center"/>
+	</a> 
+	<a href="favorites.php" target="_self" align >
+	<img src="FAVE.png" width ="80" height ="40"align ="center"/>
+	</a> 
+	<a href="trivias.php" target="_self" align >
+	<img src="TRIVIAS.png" width ="80" height ="40"align ="center"/>
+	</a>
+	<a href="contact.php" target="_self" align >
+	<img src="CONTACT.png" width ="80" height ="40"align ="center"/>
+	</a>
+	<a href="form.php" target="_self" align >
+	<img src="FORM.png" width ="80" height ="40"align ="center"/>
+	</a>
+
+</div>
+
+<?php
+include_once 'dbconfig.php';
+?>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>FORM</title>
+
+<script type="text/javascript">
+function edt_id(id)
+{
+ if(confirm('Sure to edit ?'))
+ {
+  window.location.href='edit_data.php?edit_id='+id;
+ }
+}
+function delete_id(id)
+{
+ if(confirm('Sure to Delete ?'))
+ {
+  window.location.href='index.php?delete_id='+id;
+ }
+}
+</script>
+</head>
+<body>
+<center>
+<br>
+<a class="adddata" href="form.php">add data here.</a>
+<br>
+<br>
+<div id="body">
+ <div id="content">
+    <table align="center">
+    <th>Name</th>
+    <th>Nickname</th>
+    <th>Email</th>
+	<th>Home Address</th>
+	<th>Gender</th>
+	<th>Cell Phone</th>
+	<th>Comments</th>
+    <th colspan="2">Operations</th>
+    </tr>
+    <?php
+ $sql_query="SELECT * FROM users";
+ $result_set=mysqli_query($link, $sql_query);
+ 
+ if (!$result_set) { // checks if query is correct
+    die('Invalid query: ' . mysql_error());
+	}
+ while($row=mysqli_fetch_row($result_set))
+ {
+  ?>
+        <tr>
+        <td><?php echo $row[1]; ?></td>
+        <td><?php echo $row[2]; ?></td>
+        <td><?php echo $row[3]; ?></td>
+		<td><?php echo $row[4]; ?></td>
+		<td><?php echo $row[5]; ?></td>
+		<td><?php echo $row[6]; ?></td>
+		<td><?php echo $row[7]; ?></td>
+		
+		<td align="center"><a href="javascript:edt_id('<?php echo $row[0]; ?>')"><img src="b_edit.png" align="EDIT" /></a></td>
+        <td align="center"><a href="javascript:delete_id('<?php echo $row[0]; ?>')"><img src="b_drop.png" align="DELETE" /></a></td>
+        </tr>
+        <?php
+ }
+ ?>
+    </table>
+    </div>
+</div>
+
+</center>
+</body>
+</html>
