6ced460bdda6c7237ad6c0fe71451278002e3139
Rachel Anne Brillantes <Momoxielle@gmail.com>
2016-12-19 07:07:16 +0800

    edited main.php

--- exercise7/yii/basic/views/layouts/main.php
+++ exercise7/yii/basic/views/layouts/main.php
@@ -38,7 +38,7 @@ AppAsset::register($this);
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
-            
+            ['label' => 'Users', 'url' => ['/form']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
