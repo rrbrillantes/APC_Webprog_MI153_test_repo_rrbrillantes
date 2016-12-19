0f02e6a82ce1a6a577e9f67279034c29e44e939b
Rachel Anne Brillantes <Momoxielle@gmail.com>
2016-12-19 15:33:35 +0800

    edited main.php

--- exercise7/yii/basic/views/layouts/main.php
+++ exercise7/yii/basic/views/layouts/main.php
@@ -27,7 +27,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'Rachel Fashion Corp.',
+        'brandLabel' => 'All About Me',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
@@ -36,8 +36,8 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
+            ['label' => 'Home', 'url' => ['/page/info']],
+            ['label' => 'About', 'url' => ['/page/index']],
             ['label' => 'Users', 'url' => ['/form']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
@@ -68,7 +68,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Rachel Anne R. Brillantes, WEBPROG - MI153 <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
