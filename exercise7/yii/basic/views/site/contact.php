540dc1535b9f705fe7df1c89e97cb4e56fcbb92a
Rachel Anne Brillantes <Momoxielle@gmail.com>
2016-12-22 00:50:43 +0800

    modified site/contact.php

--- exercise7/yii/basic/views/site/contact.php
+++ exercise7/yii/basic/views/site/contact.php
@@ -11,58 +11,97 @@ use yii\captcha\Captcha;
 $this->title = 'Contact';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-contact">
-    <h1><?= Html::encode($this->title) ?></h1>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
 
-    <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
+<style type="text/css">
 
-        <div class="alert alert-success">
-            Thank you for contacting us. We will respond to you as soon as possible.
-        </div>
 
-        <p>
-            Note that if you turn on the Yii debugger, you should be able
-            to view the mail message on the mail panel of the debugger.
-            <?php if (Yii::$app->mailer->useFileTransport): ?>
-                Because the application is in development mode, the email is not sent but saved as
-                a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>.
-                Please configure the <code>useFileTransport</code> property of the <code>mail</code>
-                application component to be false to enable email sending.
-            <?php endif; ?>
-        </p>
+    p.con {
+        font-size: 40px;
+        color: BLACK;
+        font-family: Gigi;
+        text-align: center;
+        font-weight: bold;
+    }
+    
+    .error {
+        color: #FF0000;
+        font-size:20px;
+    }
 
-    <?php else: ?>
+</style>
+<body>
 
-        <p>
-            If you have business inquiries or other questions, please fill out the following form to contact us.
-            Thank you.
-        </p>
+<center>
 
-        <div class="row">
-            <div class="col-lg-5">
+<p class="con">You can reach me through the following sites</p>
 
-                <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
 
-                    <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
-
-                    <?= $form->field($model, 'email') ?>
-
-                    <?= $form->field($model, 'subject') ?>
-
-                    <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
-
-                    <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
-                        'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
-                    ]) ?>
-
-                    <div class="form-group">
-                        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
-                    </div>
-
-                <?php ActiveForm::end(); ?>
-
-            </div>
-        </div>
-
-    <?php endif; ?>
-</div>
+<table class="t1" align="middle" bgcolor="#fde9ec" BORDER="1" BORDERCOLOR="BLACK" cellpadding="4" width="30%" style="float:center">
+    <tr>
+        <td> 
+        <p class="intro stylee">Facebook</p>
+        </td>
+    
+        <td>
+        
+        <center><p class="intro stylee"><a href = "https://www.facebook.com/momoxielle" target="_blank"> <img src="<?php echo Yii::$app->request->baseUrl . "/Images/fb.png";?>" height="80" align="center"></a><figcaption>Rachel Anne Brillantes</figcaption></center></p>
+        </td>
+    </tr>
+    
+    <tr>
+        <td> 
+        <p class="intro stylee">Twitter</p>
+        </td>
+        
+        <td> 
+        <center><p class="intro stylee"><a href = "https://www.twitter.com/_raimssi" target="_blank"> <img src="<?php echo Yii::$app->request->baseUrl . "/Images/twitter.png";?>" height="80" align="center"></a><figcaption>@_raimssi</figcaption></center></p>
+        </td>
+    </tr>
+    
+    <tr>
+        <td> 
+        <p class="intro stylee">Instagram</p>
+        </td>
+        
+        <td>
+        <center><p class="intro stylee"><a href = "https://www.instagram.com/_raimssi" target="_blank"> <img src="<?php echo Yii::$app->request->baseUrl . "/Images/insta.png";?>" height="80" align="center"></a><figcaption>@_raimssi</figcaption></center></p>
+        </td>
+    </tr>
+    
+    <tr>
+        <td> 
+        <p class="intro stylee">Youtube</p>
+        </td>
+        
+        <td>
+        <center><p class="intro stylee"><a href = "https://www.youtube.com/channel/UC3jO-jb9FzhcM21GJ-AY4aA" target="_blank"> <img src="<?php echo Yii::$app->request->baseUrl . "/Images/yt.png";?>" height="50" align="center"></a><figcaption>Rachel Anne Brillantes</figcaption></center></p>
+        </td>
+    </tr>
+    
+    <tr>
+        <td> 
+        <p class="intro stylee">Snapchat</p>
+        </td>
+        
+        <td>
+        <center><p class="intro stylee"><img class="net" src="<?php echo Yii::$app->request->baseUrl . "/Images/snapchat.png";?>" height="50" align="center"></a><figcaption>@momoxielle</figcaption></center></p>
+        </td>
+    </tr>
+    
+    <tr>
+        <td> 
+        <p class="intro stylee">Gmail</p>
+        </td>
+        
+        <td>
+        <center><p class="intro stylee"><img class="net" <img src="<?php echo Yii::$app->request->baseUrl . "/Images/gmail.png";?>" height="50" align="center"></a><figcaption>Momoxielle@gmail.com</figcaption></center></p>
+        </td>
+    </tr>
+</table>
+</center>
+</body>
+</html>
