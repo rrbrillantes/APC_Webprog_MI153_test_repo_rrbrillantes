30b92770b0489896b92bce6f9ba1b5b2ffa863b6
Rachel Anne Brillantes <momoxielle@gmail.com>
2016-12-22 03:06:41 +0800

    edited form/create.php

--- exercise7/yii/basic/views/form/create.php
+++ exercise7/yii/basic/views/form/create.php
@@ -6,14 +6,34 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Form */
 
-$this->title = 'Create Form';
+$this->title = 'Leave a comment';
 $this->params['breadcrumbs'][] = ['label' => 'Forms', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+
+<style type="text/css">
+
+    .form-create {
+         width: 40%;
+    }
+    
+    p.con {
+        font-size: 40px;
+        color: BLACK;
+        font-family: Gigi;
+        text-align: center;
+        font-weight: bold;
+    }
+</style>
+<center>
+<br>
+<br>
+
 <div class="form-create">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
+    <p class="con"><?= Html::encode($this->title) ?></p>
+<br>
+<br>
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
